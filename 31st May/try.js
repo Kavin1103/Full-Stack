63ba092a7eed9fda002fe20c03a01006e7601f34
Kavin1103 <srikavin1103@gmail.com>
2025-05-31 13:13:56 +0530

    31st may 1st push

---
+++ 31st May/try.js
@@ -0,0 +1,34 @@
+var num=5;
+console.log(num)
+
+const a = 'This is kavin'
+console.log(a)
+
+let b = 1*2+5/2%2
+console.log(b)
+
+let c=2.5%2
+console.log(c)
+
+console.log(5+true+true+true*3+true)// here true is considered as 1
+
+let d= 5/0
+console.log(d)
+console.log(Infinity)
+
+const name1 = 'Kavin'
+const name3 = "This"
+const name2 = `i am 
+kavin`
+//In const there can be no 2 variable with same name
+console.log(name1)
+console.log(name3)
+console.log(name2)
+
+var ab = 10//the var can redeclare the values 
+console.log(ab)
+var ab = 100
+console.log(ab)
+
+var ab = -10/0
+console.log(ab)
