2b458b2c7d60126f799ceed3e16386b89251270e
Kavin1103 <srikavin1103@gmail.com>
2025-05-31 15:27:22 +0530

    calculator js

--- 31st May/new.js
+++ 31st May/new.js
@@ -1,5 +1,10 @@
-function getdata (color='red'){
-    console.log("This is very important")
+function getdata(color = 'red') {
+    console.log("Selected color is:", color);
 }
-getdata()
 
+getdata('yellow');
+
+function getdata2() {
+    console.log("I am Kavin");
+}
+getdata2();
