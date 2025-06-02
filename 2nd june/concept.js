c4b53965dc185af45880be990ffee6fd8dc278df
Kavin1103 <srikavin1103@gmail.com>
2025-06-02 13:42:39 +0530

    2nd june 1st commit

---
+++ 2nd june/concept.js
@@ -0,0 +1,8 @@
+//Conditional statements
+// Example of an if-else statement
+let age = 20;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are a minor.");
+}
