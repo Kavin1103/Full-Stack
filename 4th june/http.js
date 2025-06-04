99c5930018c67954c2e55a8354c37ca47cec7bb7
Kavin1103 <srikavin1103@gmail.com>
2025-06-04 14:04:29 +0530

    4th june 2nd commit

---
+++ 4th june/http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Darshana is don\n');
+}).listen(8000);
+
+console.log('Server running at http://localhost:8000/');
