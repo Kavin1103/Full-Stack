147931c00113595aee2a92bf4badb245e6b8971b
Kavin1103 <srikavin1103@gmail.com>
2025-06-05 15:10:43 +0530

    5th june 4th commit

---
+++ 5th june/form.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form method="POST" action="/submit">
+            <input type="text" name="name" placeholder="Enter your name"><br>
+            <input type="email" name="email" placeholder="Enter your email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    `);
+});
+
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`Form submitted successfully!<br>Name: ${name}<br>Email: ${email}`);
+});
+
+app.listen(8000, () => {
+    console.log('Server running at http://localhost:8000/');
+});
